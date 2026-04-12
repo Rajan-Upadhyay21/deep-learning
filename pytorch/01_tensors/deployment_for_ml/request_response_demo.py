6c88f55c7692524df1d2d2ad4fded7ea53c8637d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-12 15:56:22 -0500

    Create request_response_demo.py

---
+++ pytorch/01_tensors/deployment_for_ml/request_response_demo.py
@@ -0,0 +1,16 @@
+sample_request = {
+    "feature_1": 5.2,
+    "feature_2": 3.1,
+    "feature_3": 1.8
+}
+
+sample_response = {
+    "prediction": "Class_A",
+    "confidence": 0.94
+}
+
+print("Sample Request:")
+print(sample_request)
+
+print("\nSample Response:")
+print(sample_response)
