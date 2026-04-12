db81e4a21e949140e2423e56d6ac835bb9f8a348
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-12 15:45:15 -0500

    Create monitoring_basics.py

---
+++ pytorch/01_tensors/mLOps_basics/monitoring_basics.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+predictions = np.array([0.82, 0.91, 0.77, 0.63, 0.88, 0.79])
+
+print("Prediction Statistics:")
+print("Mean:", predictions.mean())
+print("Min:", predictions.min())
+print("Max:", predictions.max())
+print("Std:", predictions.std())
