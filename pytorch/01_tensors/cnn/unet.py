6fdb2c42958a95b98c89222a388db233dfd2351f
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-12 14:49:20 -0500

    Create unet.py

---
+++ pytorch/01_tensors/cnn/unet.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1):
+        super().__init__()
+
+        self.enc1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.bottleneck = DoubleConv(128, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.enc1(x)
+        p1 = self.pool1(x1)
+
+        x2 = self.enc2(p1)
+        p2 = self.pool2(x2)
+
+        b = self.bottleneck(p2)
+
+        u2 = self.up2(b)
+        u2 = torch.cat([u2, x2], dim=1)
+        d2 = self.dec2(u2)
+
+        u1 = self.up1(d2)
+        u1 = torch.cat([u1, x1], dim=1)
+        d1 = self.dec1(u1)
+
+        out = self.final_conv(d1)
+        return out
+
+
+model = UNet(in_channels=3, out_channels=1)
+x = torch.randn(2, 3, 128, 128)
+output = model(x)
+
+print("Input shape:", x.shape)
+print("Output shape:", output.shape)
+print(model)
