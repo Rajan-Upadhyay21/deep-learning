b3cb3862a46ae186c6bb392801d82a5fe4ce3d9a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-12 14:34:07 -0500

    Create positional_encoding.py

---
+++ pytorch/01_tensors/rnn/positional_encoding.py
@@ -0,0 +1,16 @@
+import torch
+import math
+
+def positional_encoding(seq_len, d_model):
+    pe = torch.zeros(seq_len, d_model)
+    position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+    div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+    pe[:, 0::2] = torch.sin(position * div_term)
+    pe[:, 1::2] = torch.cos(position * div_term)
+    return pe
+
+pe = positional_encoding(seq_len=8, d_model=16)
+
+print("Positional Encoding Shape:", pe.shape)
+print(pe)
