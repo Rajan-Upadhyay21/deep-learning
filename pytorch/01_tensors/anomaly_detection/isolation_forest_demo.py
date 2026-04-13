33b0c3436e895fc35a6d01be2e6d5a7a6f6d856e
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-13 14:56:14 -0500

    Create isolation_forest_demo.py

---
+++ pytorch/01_tensors/anomaly_detection/isolation_forest_demo.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+
+normal_data = np.random.normal(loc=50, scale=5, size=(100, 2))
+anomalies = np.array([[90, 95], [100, 110], [85, 92]])
+
+X = np.vstack([normal_data, anomalies])
+
+model = IsolationForest(contamination=0.03, random_state=42)
+labels = model.fit_predict(X)
+
+df = pd.DataFrame(X, columns=["Feature1", "Feature2"])
+df["Label"] = labels
+
+print(df.tail(10))
