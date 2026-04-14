93c24b1ea7801cc28a12be8a0f2f92c195b9382f
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-14 14:37:12 -0500

    Create state_action_reward.py

---
+++ pytorch/01_tensors/reinforcement_learning/state_action_reward.py
@@ -0,0 +1,11 @@
+state = "S1"
+actions = ["left", "right", "up", "down"]
+chosen_action = "right"
+reward = 10
+next_state = "S2"
+
+print("Current State:", state)
+print("Available Actions:", actions)
+print("Chosen Action:", chosen_action)
+print("Reward:", reward)
+print("Next State:", next_state)
