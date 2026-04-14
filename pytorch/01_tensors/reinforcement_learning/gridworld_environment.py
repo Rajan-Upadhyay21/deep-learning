d6b51345a23d0c079ca9ac76ae24615d6a2ffaa5
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-14 14:39:06 -0500

    Create gridworld_environment.py

---
+++ pytorch/01_tensors/reinforcement_learning/gridworld_environment.py
@@ -0,0 +1,15 @@
+grid = [
+    ["S", ".", "."],
+    [".", "#", "."],
+    [".", ".", "G"]
+]
+
+print("Gridworld Environment:")
+for row in grid:
+    print(row)
+
+print("\nLegend:")
+print("S = Start")
+print("G = Goal")
+print("# = Obstacle")
+print(". = Free cell")
