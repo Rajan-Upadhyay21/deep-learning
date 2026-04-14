e2175e9b38045b91cbccd15a3ce83fb294b7074c
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-14 11:23:25 -0500

    Create image_captioning_concept.py

---
+++ pytorch/01_tensors/multimodal_ai/image_captioning_concept.py
@@ -0,0 +1,13 @@
+image_description_features = {
+    "objects": ["dog", "ball", "grass"],
+    "scene": "park",
+    "action": "playing"
+}
+
+caption = "A dog is playing with a ball in the park."
+
+print("Image Features:")
+print(image_description_features)
+
+print("\nGenerated Caption:")
+print(caption)
