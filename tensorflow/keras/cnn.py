3bcfa3f698d5b8ade03da3021c95e85bdf67c48a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-18 20:49:53 -0500

    Create cnn.py

---
+++ tensorflow/keras/cnn.py
@@ -0,0 +1,29 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train[..., None] / 255.0
+X_test = X_test[..., None] / 255.0
+
+model = keras.Sequential([
+    layers.Conv2D(16, (3, 3), activation="relu", input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(X_train, y_train, epochs=2, verbose=1)
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print("Test Accuracy:", accuracy)
